91c876fb2d5cf26b037201f6bcd6db4a70d93703
shaode <shaode2012@sina.cn>
2015-09-17 20:52:32 +0800

    页面布局初始化样式加载

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
-
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : ''
+});
 /* GET home page. */
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: ' NodeJS Express Home' });
+    //res.render('index', { title: ' NodeJS Express Home' ,message:"test"});
+    connection.connect();
+    connection.query('select * from walle_admin.users', function(err, rows, fields) {
+        if (err) throw err;
+        res.render('index', { title: ' NodeJS Express Home' ,users:rows});
+    });
+    connection.end();
+
 });
 
 module.exports = router;
